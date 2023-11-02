e3de09957f963b58829b15766b7f33888baad7ad
Stimmi86 <stimmi86@gmail.com>
2023-11-02 23:06:46 +0000

    verk 8

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-import { createCartLine, showCartContent } from './lib/ui.js';
+import { createCartLine, showCartContent, updateCartTotal } from './lib/ui.js';
+import { formatNumber } from './lib/helpers.js';
 
 const products = [
   {
@@ -22,32 +23,41 @@ const products = [
   },
 ];
 
+let cartItems = {};
 /** Bæta vöru í körfu */
 function addProductToCart(product, quantity) {
-  // Hér þarf að finna `<tbody>` í töflu og setja `cartLine` inn í það
-  const cart = document.querySelector('.cart-content');
+  const productId = product.id;
+  const cartItemElement = document.querySelector(`.addToCart tr[data-cart-product-id="${productId}"]`);
 
-  if (!cart) {
-    console.warn('fann ekki .cart');
-    return;
-  }
-  
   // TODO hér þarf að athuga hvort lína fyrir vöruna sé þegar til
-  const cartLine = createCartLine(product, quantity);
-  cart.appendChild(cartLine);
+  if (cartItemElement) {
+    const quantityCell = cartItemElement.querySelector('td:nth-child(2)');
+    const totalCell = cartItemElement.querySelector('td:nth-child(4) span.price');
+
+    if (quantityCell && totalCell) {
+      quantityCell.textContent = parseInt(quantityCell.textContent) + quantity;
+      const newTotalPrice = product.price * parseInt(quantityCell.textContent);
+      totalCell.textContent = formatNumber(newTotalPrice);
+    }
+  } else {
+    cartItems[productId] = quantity;
+    const cart = document.querySelector('.addToCart');
+    const cartLine = createCartLine(product, quantity, updateCartTotal);
+    cart.appendChild(cartLine);
+  }
 
   // Sýna efni körfu
   showCartContent(true);
-
   // TODO sýna/uppfæra samtölu körfu
+  updateCartTotal();
 }
 
 function submitHandler(event) {
   // Komum í veg fyrir að form submiti
   event.preventDefault();
-  
+
   // Finnum næsta element sem er `<tr>`
-  const parent = event.target.closest('tr')
+  const parent = event.target.closest('tr');
 
   // Það er með attribute sem tiltekur auðkenni vöru, t.d. `data-product-id="1"`
   const productId = Number.parseInt(parent.dataset.productId);
@@ -57,14 +67,12 @@ function submitHandler(event) {
 
   // TODO hér þarf að finna fjölda sem á að bæta við körfu með því að athuga
   // á input
-  const quantity = 1;
-
-  // Bætum vöru í körfu (hér væri gott að bæta við athugun á því að varan sé til)
+  const quantity = parseInt(parent.querySelector('.quantityInput').value);
   addProductToCart(product, quantity);
 }
 
 // Finna öll form með class="add"
-const addToCartForms = document.querySelectorAll('.add')
+const addToCartForms = document.querySelectorAll('.add');
 
 // Ítra í gegnum þau sem fylki (`querySelectorAll` skilar NodeList)
 for (const form of Array.from(addToCartForms)) {
@@ -73,3 +81,43 @@ for (const form of Array.from(addToCartForms)) {
 }
 
 // TODO bæta við event handler á form sem submittar pöntun
+const completePurchaseBtn = document.getElementById('completePurchaseBtn');
+
+completePurchaseBtn.addEventListener('click', function (event) {
+  event.preventDefault();
+
+  const name = document.getElementById('name').value;
+  const address = document.getElementById('address').value;
+
+  const cartItems = document.querySelectorAll('.addToCart tr');
+
+  let receiptContent = `<h2>Kvittun</h2>`;
+  receiptContent += `<p>Nafn: ${name}</p>`;
+  receiptContent += `<p>Heimilisfang: ${address}</p>`;
+  receiptContent += `<table><thead><tr><th>Heiti</th><th>Fjöldi</th><th>Verð</th></tr></thead><tbody>`;
+
+  let totalSum = 0;
+  cartItems.forEach((item) => {
+    const productName = item.querySelector('td:nth-child(1)').textContent;
+    const quantity = parseInt(item.querySelector('td:nth-child(2)').textContent);
+    const price = parseInt(item.querySelector('td:nth-child(3) span.price').textContent.replace('.', ''), 10);
+    const totalPrice = price * quantity;
+    totalSum += totalPrice;
+
+    receiptContent += `<tr><td>${productName}</td><td>${quantity}</td><td>${formatNumber(totalPrice)}</td></tr>`;
+  });
+
+  receiptContent += `</tbody></table>`;
+  receiptContent += `<p><strong>Samtals: ${formatNumber(totalSum)}</strong></p>`;
+  receiptContent += `<p><a href=".">Kaupa meira</a>.</p>`;
+
+  const receiptSection = document.querySelector('.receipt');
+  receiptSection.innerHTML = receiptContent;
+
+  showCartContent(false);
+  receiptSection.querySelector('p').textContent = `Takk fyrir að versla hjá okkur, ${name}!`;
+
+  document.querySelector('.addToCart').innerHTML = '';
+
+  updateCartTotal();
+});
